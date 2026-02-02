e77a817f2e43c6eceb8c21e737b6c8c949126036
Ryan <ryanoliver565@gmail.com>
2026-02-02 09:29:31 +0800

    feat: add csp headers

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// dev
-const supabaseUrl = 'https://tsrbomktxkdkklfyfnav.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzcmJvbWt0eGtka2tsZnlmbmF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4MTc5NDcsImV4cCI6MjA4NDM5Mzk0N30.hTRAjgWXHIB3OZP-kBxtr6ig8o1dxU82pjPLGPIfAb4'
-
-// const supabaseUrl = 'https://qaeeziiiyhfmhljpiopr.supabase.co'
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhZWV6aWlpeWhmbWhsanBpb3ByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxODc5MTAsImV4cCI6MjA4NDc2MzkxMH0.TcE5_VPV59R-GSaTNq70NS1nqbnEzwm2UhGxGOKNkNg'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
